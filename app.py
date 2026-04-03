79e40178fceb625c9006d63fd51bbe333a8bf947
Dania Midera <daniamidera@gmail.com>
2026-04-04 00:32:31 +0400

    Fix flake8 style violations

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 def bonjour():
     return "Bonjour, monde!"
 
+
 def calculer_remise(prix, taux):
     return prix - (prix * taux)
+
 
 def calculer_tva(prix, taux=0.20):
     return prix + (prix * taux)
